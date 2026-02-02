db7d7e316bb8756d420f1e255a1534203f178cc4
Rüthemann Peter <peter.ruethemann@gmail.com>
2026-02-02 11:50:04 +0100

    Update license foldX

--- install/install_bins_linux.sh
+++ install/install_bins_linux.sh
@@ -4,7 +4,7 @@
 
 # Download archive containing all bins
 echo "Download software in current folder"
-curl -L -o squeezemd-bin.tar.gz "https://www.dropbox.com/scl/fi/ngo4nhh0mcj09goob10wk/squeezemd-bin.tar.gz?rlkey=1kw69tmcrrgmqzbx096ksd7l1&st=ouhq22rf&dl=1"
+curl -L -o squeezemd-bin.tar.gz "https://www.dropbox.com/scl/fi/vrsujfeh4ka11xdfgt6b4/squeezemd-bin.tar.gz?rlkey=ibj0f6pxez6hcfooowqwvzdsr&st=ff0w04s5&dl=1"
 
 # Unpack archiv in ~/tools/
 INSTALLDIR=~/tools/
@@ -13,7 +13,7 @@ tar -xvf squeezemd-bin.tar.gz -C $INSTALLDIR
 
 # Save paths in bashrc (extended bash on current Mac)
 echo "# foldX
-export PATH=\$PATH:~/tools/foldX/foldx5_1Linux64_0" >> ~/.bashrc
+export PATH=\$PATH:~/tools/foldX/foldx_Linux" >> ~/.bashrc
 
 # PosCo
 echo "# Po-Sco
